a6abaa0e048abb898f4e08683be49e3c67408214
Preston Shipman <pshipman2000@gmail.com>
2019-11-14 11:26:07 -0800

    Got cards to match and flip back over

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(initializeApp)
+$(document).ready(initializeApp);
 function initializeApp() {
   $('.card').on('click', handleCardClick);//when user  clicks on the selected jquery dom, call the handleCardClick function
 }
@@ -8,28 +8,24 @@ var matches = null;
 
 
 function handleCardClick(event) {
-  var clickedCard = event.currentTarget;
-  console.log("only cardimg child: ", $(clickedCard).find('.cardimg'))
-  $(clickedCard).find('.cardimg').addClass('hidden')
-if (firstCardClicked === null) {
-  firstCardClicked = event.currentTarget;
-}
-else {
-  secondCardClicked = event.currentTarget;
-}
-var firstFace = firstCardClicked.find('.face').css('background-image');
-var secondFace = secondCardClicked.find('.face').css('background-image');
-console.log(firstCardClicked);
+  $(event.currentTarget).find('.cardimg').addClass('hidden');
+  console.log("only cardimg child: ", event.currentTarget)
 
-//console.log(firstFace);
-//console.log(secondFace);
-if (firstFace==secondFace) {
-  console.log("Cards Match");
-  matches +1;
+  if (firstCardClicked === null) {
+    firstCardClicked = $(event.currentTarget);
+  }
+  else {
+    secondCardClicked = $(event.currentTarget);
+    var firstFace = firstCardClicked.find('.face').css('background-image');
+    var secondFace = secondCardClicked.find('.face').css('background-image');
+    if (firstFace === secondFace) {
+      console.log('They match');
+      matches+1;
+    } else {
+      setTimeout(function () {
+        firstCardClicked.find('.cardimg').removeClass('hidden');
+        secondCardClicked.find('.cardimg').removeClass('hidden');
+      }, 1500);
+    }
+  }
 }
-}
-// play around with arrangement of code and give secondFace a value.
-// Secondface wont work since its in an else statement therefore since the first
-// if is true else never runs therefore it has no value
-// This is why im getting the errors. Then after you fix this cocnsole log
-// first face and second face.
