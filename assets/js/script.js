ae8b4318b587ad741fc62ca050cba1c6b978a7cc
Preston Shipman <pshipman2000@gmail.com>
2019-11-14 19:46:45 -0800

    Made the modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,12 @@
 $(document).ready(initializeApp);
 function initializeApp() {
   $('.card').on('click', handleCardClick);//when user  clicks on the selected jquery dom, call the handleCardClick function
+  $('.modal').addClass('hidden');
 }
 var firstCardClicked = null;
 var secondCardClicked = null;
 var matches = null;
+var max_matches = 2;
 
 
 function handleCardClick(event) {
@@ -20,11 +22,19 @@ function handleCardClick(event) {
     var secondFace = secondCardClicked.find('.face').css('background-image');
     if (firstFace === secondFace) {
       console.log('They match');
-      matches+1;
+      matches++;
+      if (matches === max_matches) {
+        $('#myModal').removeClass('hidden');
+        console.log('the if modal runs');
+      }
+      firstCardClicked = null;
+      secondCardClicked = null;
     } else {
       setTimeout(function () {
         firstCardClicked.find('.cardimg').removeClass('hidden');
         secondCardClicked.find('.cardimg').removeClass('hidden');
+        firstCardClicked = null;
+        secondCardClicked = null;
       }, 1500);
     }
   }
