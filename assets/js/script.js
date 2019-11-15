992710dc6ec3db605b594b6e74e7bcdd7c615397
Preston Shipman <pshipman2000@gmail.com>
2019-11-15 15:53:04 -0800

    Got the stats working

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,13 +6,14 @@ function initializeApp() {
 var firstCardClicked = null;
 var secondCardClicked = null;
 var matches = null;
-var max_matches = 2;
+var max_matches = 9;
+var attempts = null;
+var games_played = null;
 
 
 function handleCardClick(event) {
   $(event.currentTarget).find('.cardimg').addClass('hidden');
   console.log("only cardimg child: ", event.currentTarget)
-
   if (firstCardClicked === null) {
     firstCardClicked = $(event.currentTarget);
   }
@@ -26,6 +27,7 @@ function handleCardClick(event) {
       if (matches === max_matches) {
         $('#myModal').removeClass('hidden');
         console.log('the if modal runs');
+        games_played += 1;
       }
       firstCardClicked = null;
       secondCardClicked = null;
@@ -38,4 +40,16 @@ function handleCardClick(event) {
       }, 1500);
     }
   }
+  attempts += 1;
+  displayStats();
+}
+function calculateAccuracy() {
+   var accOutput = (attempts / matches);
+  return accOutput;
+}
+function displayStats() {
+  $('.stats1').text(attempts);
+  var accuracy = calculateAccuracy();
+  $('.stats2').text(accuracy);
+  $('.stats6').text(games_played);
 }
